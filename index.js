f5c30fa90c90176dcd6f4490c9d9e7fc36555dac
Brian Pluhar <pluharbrian@gmail.com>
2023-08-23 02:19:34 -0400

    Server Sided Crypto RNG

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const crypto = require('crypto');
+
 require('dotenv').config();
 
 var colors = require('colors');
+var terminal = require('terminal-kit').terminal;
 
 function ifThrow(error) {
   if (error) throw error;
@@ -24,14 +27,34 @@ function loadLast(res) {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); 
+  clientSendFile(res, "/index.html");
 });
 
+app.get('/home', (req, res) => {
+  clientSendFile(res, "/index.html");
+});
 
+function clientSendFile(res, filename) {
+  res.sendFile(__dirname + filename);
+  //console.log(`Sending File: ${filename}`.yellow);
+  terminal('Sending File: ').yellow(filename)('\n');
+}
+
+function getRandomInt(min, max) {       
+    // Create byte array and fill with 1 random number
+    var byteArray = new Uint8Array(1);
+    crypto.getRandomValues(byteArray);
+
+    var range = max - min + 1;
+    var max_range = 256;
+    if (byteArray[0] >= Math.floor(max_range / range) * range)
+      return getRandomInt(min, max);
+    return min + (byteArray[0] % range);
+  }
 
 io.on('connection', (socket) => {
 
-  console.log('A user connected'.brightCyan);
+  terminal.cyan('A user has connected\n');
 
   // on 'chat message' from client to server
   socket.on('chat message', (msg) => {
@@ -57,20 +80,27 @@ io.on('connection', (socket) => {
     io.emit('chat message', msg); 
   });
 
+  socket.on('debug', () => {
+    let start = Date.now();
+
+    count = 10001;
+
+    for (let x = 0; x < count; x++) {
+      //let rng = getRandomInt(0, 100);
+      io.emit('debug', x);
+    };
+    let delta = Date.now() - start;
+    terminal(`Took `).green(delta)('ms to generate and emit ').green(count - 1)(' numbers to all ').green(io.engine.clientsCount)(' sockets.\n');
+  })
+
   socket.on('disconnect', () => {
-    console.log('A user disconnected'.brightRed);
+    //console.log('A user disconnected'.brightRed);
+    terminal.red('A User Disconnected!\n');
   });
 
-  socket.on('client_startTyping', () => {
-    console.log("User is Typing!");
-  });
-
-  socket.on('client_stopTyping', () => {
-    console.log("User stopped typing!");
-  });
   
 });
 
 server.listen(process.env.port, () => {
-  console.log(`Server listening on *:${process.env.port}`);
+  terminal.white('Server listening on port: ').green(`${process.env.port}\n`);
 });
